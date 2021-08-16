ae61c1781abcfe1e9ebd5b742cb6a25215495119
mutiara <mutiaranis17@gmail.com>
2021-08-16 20:22:35 +0700

    fix bugs host value

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -17,11 +17,19 @@ class SongsHandler {
     try {
       this._validator.validateSongPayload(request.payload);
       const {
-        title = 'untitled', year, performer, genre, duration,
+        title,
+        year,
+        performer,
+        genre,
+        duration,
       } = request.payload;
 
       const songId = await this._service.addSong({
-        title, year, performer, genre, duration,
+        title,
+        year,
+        performer,
+        genre,
+        duration,
       });
 
       const response = h.response({
@@ -54,18 +62,38 @@ class SongsHandler {
     }
   }
 
-  async getSongsHandler() {
-    const songs = await this._service.getSongs();
-    return {
-      status: 'success',
-      data: {
-        songs: songs.map((song) => ({
-          id: song.id,
-          title: song.title,
-          performer: song.performer,
-        })),
-      },
-    };
+  async getSongsHandler(request, h) {
+    try {
+      const songs = await this._service.getSongs();
+      return {
+        status: 'success',
+        data: {
+          songs: songs.map((song) => ({
+            id: song.id,
+            title: song.title,
+            performer: song.performer,
+          })),
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 
   async getSongByIdHandler(request, h) {
--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -11,7 +11,11 @@ class SongsService {
   }
 
   async addSong({
-    title, year, performer, genre, duration,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
   }) {
     const id = nanoid(16);
     const insertedAt = new Date().toISOString();
@@ -19,7 +23,16 @@ class SongsService {
 
     const query = {
       text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
-      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+      values: [
+        id,
+        title,
+        year,
+        performer,
+        genre,
+        duration,
+        insertedAt,
+        updatedAt,
+      ],
     };
 
     const result = await this._pool.query(query);
@@ -32,7 +45,7 @@ class SongsService {
   }
 
   async getSongs() {
-    const result = await this._pool.query('SELECT * FROM songs');
+    const result = await this._pool.query('SELECT id, title, performer FROM songs');
     return result.rows.map(mapDBToModel);
   }
 
@@ -51,12 +64,24 @@ class SongsService {
   }
 
   async editSongById(id, {
-    title, year, performer, genre, duration,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
   }) {
     const updatedAt = new Date().toISOString();
     const query = {
       text: 'UPDATE songs SET title = $1, year = $2, performer = $3, genre = $4, duration = $5, updated_at = $6 WHERE id = $7 RETURNING id',
-      values: [title, year, performer, genre, duration, updatedAt, id],
+      values: [
+        title,
+        year,
+        performer,
+        genre,
+        duration,
+        updatedAt,
+        id,
+      ],
     };
 
     const result = await this._pool.query(query);
